1a9c201550e8e752da2fa45229c4d9e2f1b26c4b
Alexander B. Deyto <alex.b.deyto@gmail.com>
2021-05-14 16:30:24 +0800

    . added spinner (progress indicator) when retrieving clients per page from service. . added isolated css for clients listing page.

--- LEXEnprise.Blazor.Client/Pages/ClientsList.razor.cs
+++ LEXEnprise.Blazor.Client/Pages/ClientsList.razor.cs
@@ -4,6 +4,7 @@ using LEXEnprise.Blazor.Application.Services.Clients;
 using LEXEnprise.Blazor.Shared.Wrapper;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Configuration;
+using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +30,10 @@ namespace LEXEnprise.Blazor.Clients.Pages
         [Inject]
         public HttpInterceptorService Interceptor { get; set; }
 
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+        private IJSObjectReference _jsModule;
+
         public PageMetaData PageMetaData { get; set; } = new PageMetaData();
 
         public List<GetClientResponse> Clients = new List<GetClientResponse>();
@@ -42,10 +47,20 @@ namespace LEXEnprise.Blazor.Clients.Pages
             _getClientsRequest.PageSize = int.Parse(Config["PaginationSettings:PageSize"]);
 
             await GetClients();
+            await HideSpinner();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _jsModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./js/clients/clients.js");
+            }
         }
 
         private async Task GetClients()
         {
+            //Clients.Clear();
             var response = await ClientService.GetClients(_getClientsRequest);
 
             if (response.Succeeded)
@@ -62,8 +77,22 @@ namespace LEXEnprise.Blazor.Clients.Pages
         private async Task SelectedPage(int page)
         {
             _getClientsRequest.PageNumber = page;
-            await GetClients();
+            
+            await DisplaySpinner();
+            try
+            {
+                await GetClients();
+            }
+            finally
+            {
+                await HideSpinner();
+            }
         }
+
+        //private async Task DisplaySpinner() => await _jsModule.InvokeVoidAsync("displaySpinner", _spinnerRef);
+        private async Task DisplaySpinner() => await _jsModule.InvokeVoidAsync("displaySpinner", "spinner");
+
+        private async Task HideSpinner() => await _jsModule.InvokeVoidAsync("hideSpinner", "spinner");
 
         public void Dispose() => Interceptor.DisposeEvent();
     }
---
+++ LEXEnprise.Blazor/wwwroot/js/clients/clients.js
@@ -0,0 +1,13 @@
+﻿export function displaySpinner(id) {
+    var element = document.getElementById(id);
+
+    if (element != null)
+        element.style.display = "block";
+}
+
+export function hideSpinner(id) {
+    var element = document.getElementById(id);
+
+    if (element != null)
+        element.style.display = "none";
+}
